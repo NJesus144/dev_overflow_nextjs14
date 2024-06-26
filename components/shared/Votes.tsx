522453bc6211c41d1b1ca5c6d72167ee9e534266
NJesus144 <nalbert-edm@outlook.com>
2024-06-25 22:11:24 -0300

    create votes component and integrate into Question page

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,8 +1,81 @@
-import React from 'react'
+'use client'
 
-const Votes = () => {
+import Image from 'next/image'
+import React from 'react'
+import { formatAndDivideNumber } from '../../lib/utils'
+
+interface Props {
+  type: string
+  itemId: string
+  userId: string
+  upvotes: number
+  hasupVoted: boolean
+  downvotes: number
+  hasdownVoted: boolean
+  hasSaved?: boolean
+}
+
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved
+}: Props) => {
+
+  const handleSave = () => { }
+
+  const handleVote = (action: string) => {
+    // handle voting logic
+  }
+
   return (
-    <div>Votes</div>
+    <div className='flex gap-5'>
+      <div className='flex-center gap-2.5'>
+        <div className='flex-center gap-1.5'>
+          <Image
+            src={hasupVoted ? '/assets/icons/upvoted.svg' : '/assets/icons/upvote.svg'}
+            height={18}
+            width={18}
+            alt='upvote'
+            className='cursor-pointer'
+            onClick={() => handleVote('upvote')}
+          />
+          <div className='flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1'>
+            <p className='subtitle-medium text-dark400_light900'>
+              {formatAndDivideNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+
+        <div className='flex-center gap-1.5'>
+          <Image
+            src={hasdownVoted ? '/assets/icons/downvoted.svg' : '/assets/icons/downvote.svg'}
+            height={18}
+            width={18}
+            alt='downvote'
+            className='cursor-pointer'
+            onClick={() => handleVote('dowvote')}
+          />
+          <div className='flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1'>
+            <p className='subtitle-medium text-dark400_light900'>
+              {formatAndDivideNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <Image
+        src={hasSaved ? '/assets/icons/star-filled.svg' : '/assets/icons/star-red.svg'}
+        height={18}
+        width={18}
+        alt='star'
+        className='cursor-pointer'
+        onClick={handleSave}
+      />
+    </div>
   )
 }
 
