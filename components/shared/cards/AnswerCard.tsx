8a76036a62e3af4d8761c12f4237c1669346d4e5
NJesus144 <nalbert-edm@outlook.com>
2024-08-25 17:54:22 -0300

    add AnswerCard component

---
+++ components/shared/cards/AnswerCard.tsx
@@ -0,0 +1,85 @@
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../RenderTag";
+import Metric from "../Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+
+interface AnswerProps {
+  _id: string;
+  title: string;
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+  };
+  upvotes: string[];
+  views: number;
+  answers: Array<object>;
+  createdAt: Date;
+  clerkId?: string | null;
+}
+
+const AnswerCard = ({
+  _id,
+  title,
+  author,
+  upvotes,
+  createdAt,
+}: AnswerProps) => {
+
+  return (
+    <div className="card-wrapper rounded-[10] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimestamp(createdAt)}
+          </span>
+          <Link href={`/question/${_id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+
+        {/* If signed in add edit delete actions */}
+      </div>
+
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl={author.picture}
+          alt="user"
+          value={author.name}
+          title={` - asked ${getTimestamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light800"
+        />
+
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={formatAndDivideNumber(upvotes.length)}
+          title="Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        {/* <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(answers.length)}
+          title="Answers"
+          textStyles="small-medium text-dark400_light800"
+        /> */}
+
+        {/* <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(views)}
+          title="views"
+          textStyles="small-medium text-dark400_light800"
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default AnswerCard;
