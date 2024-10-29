9b82de8f66f1e3744096477a1bb9d1c195bb5d2a
NJesus144 <nalbert-edm@outlook.com>
2024-10-28 21:31:24 -0300

    Refactor answer.action.ts and add deleteAnswer function

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -1,23 +1,30 @@
-"use server";
+"use server"
 
-import { AnswerVoteParams, CreateAnswerParams, GetAnswersParams } from "./shared.types.d";
-import { connectToDatabase } from "../mogoose";
-import Answer from "@/database/answer.model";
-import { revalidatePath } from "next/cache";
-import Question from "@/database/question.model";
-import User from "@/database/user.model"; 
+import {
+  AnswerVoteParams,
+  CreateAnswerParams,
+  DeleteAnswerParams,
+  GetAnswersParams,
+} from "./shared.types.d"
+import { connectToDatabase } from "../mogoose"
+import Answer from "@/database/answer.model"
+import { revalidatePath } from "next/cache"
+import Question from "@/database/question.model"
+import User from "@/database/user.model"
+import { Tag } from "lucide-react"
+import Interaction from "../../database/interaction.model"
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
-    connectToDatabase();
+    connectToDatabase()
 
-    const { content, question, author, path } = params;
+    const { content, question, author, path } = params
 
-    const newAnswer = await Answer.create({ content, author, question });
+    const newAnswer = await Answer.create({ content, author, question })
 
     await Question.findByIdAndUpdate(question, {
       $push: { answers: newAnswer._id },
-    });
+    })
 
     // const answer = await Answer.create({
     //   content,
@@ -25,27 +32,27 @@ export async function createAnswer(params: CreateAnswerParams) {
     //   author,
     // });
 
-    revalidatePath(path);
+    revalidatePath(path)
   } catch (error) {
-    console.log(error);
-    throw error;
+    console.log(error)
+    throw error
   }
 }
 
 export async function getAnswers(params: GetAnswersParams) {
   try {
-    connectToDatabase();
+    connectToDatabase()
 
-    const { questionId } = params;
+    const { questionId } = params
 
-    const answers = await Answer.find({question: questionId})
+    const answers = await Answer.find({ question: questionId })
       .populate("author", "_id clerkId name picture")
-      .sort({ createdAt: -1 });
-    
-      return { answers };
+      .sort({ createdAt: -1 })
+
+    return { answers }
   } catch (error) {
-    console.log(error);
-    throw error;
+    console.log(error)
+    throw error
   }
 }
 
@@ -73,7 +80,7 @@ export async function upvoteAnswer(params: AnswerVoteParams) {
     })
 
     if (!answer) {
-      throw new Error('Answer not found')
+      throw new Error("Answer not found")
     }
 
     revalidatePath(path)
@@ -107,12 +114,37 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
     })
 
     if (!answer) {
-      throw new Error('Answer not found')
+      throw new Error("Answer not found")
     }
 
     revalidatePath(path)
   } catch (error) {
     console.log(error)
     throw error
+  }
+}
+
+export async function deleteAnswer(params: DeleteAnswerParams) {
+  try {
+    connectToDatabase()
+
+    const { answerId, path } = params
+
+    const answer = await Answer.findById(answerId)
+
+    if (!answer) {
+      throw new Error("Answer not found")
+    }
+
+    await answer.deleteOne({ _id: answerId })
+    await Question.updateMany(
+      { _id: answer.question },
+      { $pull: { answers: answerId } },
+    )
+    await Interaction.deleteMany({ answer: answerId })
+
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
   }
 }
