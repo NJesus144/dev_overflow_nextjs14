b4d1495aa10f62b7ef6c728e3276ed0e994282c1
NJesus144 <nalbert-edm@outlook.com>
2024-11-30 20:43:02 -0300

    Add filter functionality to Home and shared Filter components for improved user experience

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -1,3 +1,4 @@
+"use client";
 
 import React from "react";
 
@@ -10,6 +11,9 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+import { formUrlQuery } from "../../lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+
 interface Props {
   filters: { name: string; value: string }[];
   otherClasses?: string;
@@ -17,10 +21,28 @@ interface Props {
 }
 
 const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  const paramFilter = searchParams.get('filter')
+
+  const handleUpdateParams = (value: string) => {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: 'filter',
+      value
+    })
+
+    router.push(newUrl, { scroll: false })
+  }
+
   return (
     <>
       <div className={`relative ${containerClasses}`}>
-        <Select>
+        <Select
+          onValueChange={handleUpdateParams}
+          defaultValue={paramFilter || ''}
+        >
           <SelectTrigger
             className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
           >
@@ -32,7 +54,9 @@ const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
           <SelectContent>
             <SelectGroup>
               {filters.map((item) => (
-                <SelectItem key={item.value} value={item.value}>
+                <SelectItem key={item.value}
+                  value={item.value}
+                >
                   {item.name}
                 </SelectItem>
               ))}
