2d680e0b9ead8c1d17cc56784e3b29d8580ac188
NJesus144 <nalbert-edm@outlook.com>
2024-12-14 15:30:22 -0300

    Implement toast notifications with useToast hook and integrate Toaster in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,8 @@
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from '@/components/shared/RightSidebar';
-import Navbar from "@/components/shared/navbar/Navbar";
-import React from "react";
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
+import Navbar from "@/components/shared/navbar/Navbar"
+import React from "react"
+import { Toaster } from "@/components/ui/toaster"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -14,9 +15,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </section>
         <RightSidebar />
       </div>
-      Toaster
+      <Toaster />
     </main>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
